d9898ca5de74e645e1a72820d7a961ddaeed4b8d
elvertoni <elvertoni@gmail.com>
2024-12-15 08:00:38 -0300

    commit1

---
+++ Cap_02/Cap_02_Ex_01.py
@@ -0,0 +1,10 @@
+#Converta as seguintes expressões matemáticas para que possam ser calculadas usando o interpretador Python. 10 + 20 × 30 4^2 ÷ 30 (9^4 + 2) × 6 - 1
+
+expressao1 = 10 + 20 * 30
+print(expressao1)
+
+expressao2 =  4**2 / 30
+print(expressao2)
+
+expressao3 = (9**4 + 2) * 6 - 1
+print(expressao3)
