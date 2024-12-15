76c9277212b258827a37a6654b2feffefe49c27d
elvertoni <elvertoni@gmail.com>
2024-12-15 08:19:16 -0300

    commit2

---
+++ Cap_02/Cap_02_Ex_05.py
@@ -0,0 +1,6 @@
+# Escreva um programa que calcule a soma de três variáveis e imprima o resultado na tela.
+
+a = 10
+b = 20
+c = 30
+print(a + b + c)
---
+++ Cap_02/Cap_02_Ex_06.py
@@ -0,0 +1,5 @@
+# Escreva um programa que calcule um aumento de 15% para um salário de R$ 750
+
+salario = 750
+aumento = 15/100
+print(salario + (salario * aumento))
